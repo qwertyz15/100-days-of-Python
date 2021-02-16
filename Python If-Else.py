db7506d591f7b899da8c3ebb6ebc8291ca7ef467
Fahim Shahriar <imnirobs15@gmail.com>
2021-02-16 10:49:25 +0000

    done

--- Python If-Else.py
+++ Python If-Else.py
@@ -1,1 +1,10 @@
-!pip install pandas
+x = int(input())
+if(x % 2 == 1):
+  print('Weird')
+else:
+  if(x >=2 and x <= 5):
+    print('Not Weird')
+  elif(x >= 6 and x <=20):
+    print('Weird')
+  elif(x >= 20):
+    print('Not Weird')
